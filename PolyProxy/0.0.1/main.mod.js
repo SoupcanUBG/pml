073acc9ff0c93d46c5c3f33db51cd4480aec99c7
Soupcan <sudo.toby@outlook.com>
2026-04-14 21:36:21 +0100

    Update main.mod.js

--- PolyProxy/0.0.1/main.mod.js
+++ PolyProxy/0.0.1/main.mod.js
@@ -1,17 +1,20 @@
-import { PolyMod, MixinType, } from "https://cdn.polymodloader.com/cb/PolyTrackMods/PolyModLoader/0.6.0/PolyTypes.js";
-
+import {
+  PolyMod,
+  MixinType,
+} from "https://cdn.polymodloader.com/cb/PolyTrackMods/PolyModLoader/0.6.0/PolyTypes.js";
 
 class PolyProxy extends PolyMod {
-    preInit = (pml) => {
-      this.url = "https://polyproxy.polymodloader.com/";
-      pml.registerGlobalMixin({
-        type: MixinType.REPLACEBEWEEN,
-        tokenStart: `https://vps.kodub.com/`,
-        tokenEnd: `https://vps.kodub.com/`,
-        func: this.url,
-        });
-      //  console.log("PolyProxy loaded\n URL:" + proxyUrl);
-    };
+  preInit = (pml) => {
+    this.url = "https://polyproxy.polymodloader.com/";
+    pml.registerGlobalMixin({
+      type: MixinType.REPLACEBETWEEN,
+      tokenStart: `https://vps.kodub.com/`,
+      tokenEnd: `https://vps.kodub.com/`,
+      func: this.url,
+    });
+  };
 }
 
-export let polyMod = new PolyProxy();
+export let polyMod = new PolyProxyMod();
+
+export let polyMod = new PolyProxyMod();
