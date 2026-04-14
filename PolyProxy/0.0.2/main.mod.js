ded9fdb5250fdb208ef5e72d75637a24abf103a1
NotRx <Anonymous@proton.me>
2026-04-14 21:38:33 +0100

    '

---
+++ PolyProxy/0.0.2/main.mod.js
@@ -0,0 +1,19 @@
+import {
+  PolyMod,
+  MixinType,
+} from "https://cdn.polymodloader.com/cb/PolyTrackMods/PolyModLoader/0.6.0/PolyTypes.js";
+
+class PolyProxy extends PolyMod {
+  preInit = (pml) => {
+    this.url = "https://polyproxy.polymodloader.com/";
+    pml.registerGlobalMixin({
+      type: MixinType.REPLACEBETWEEN,
+      tokenStart: `https://vps.kodub.com/`,
+      tokenEnd: `https://vps.kodub.com/`,
+      func: this.url,
+    });
+  };
+}
+
+export let polyMod = new PolyProxyMod();
+
