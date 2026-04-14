18f36a6dada95b106d715f7e486fc5c6660486ee
NotRx <Anonymous@proton.me>
2026-04-14 20:45:01 +0100

    please

---
+++ PolyProxy/0.0.1/main.mod.js
@@ -0,0 +1,17 @@
+import { PolyMod, MixinType } from "https://cdn.polymodloader.com/cb/PolyTrackMods/PolyModLoader/0.6.0/PolyTypes.js";
+
+
+class PastesDevImport extends PolyMod {
+    preInit = (pml) => {
+      proxyURL = "https://polyproxy.polymodloader.com/";
+       pml.registerGlobalMixin({
+            type: MixinType.REPLACEBEWEEN,
+            tokenStart: `https://vps.kodub.com/`,
+            tokenEnd: `https://vps.kodub.com/`,
+            func: ProxyUrl,
+        })
+        console.log("PolyProxy loaded\n URL:" + proxyUrl)
+    }
+}
+
+export let polyMod = new PastesDevImport();
