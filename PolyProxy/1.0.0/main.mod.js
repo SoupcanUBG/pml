8f2e7cf59c9fa31bf904b54b12f03faa35952afd
NotRx <Anonymous@proton.me>
2026-04-15 11:37:53 +0100

    £oingore

--- PolyProxy/1.0.0/main.mod.js
+++ PolyProxy/1.0.0/main.mod.js
@@ -3,24 +3,22 @@ import {
   MixinType,
   SettingType,
 } from "https://cdn.polymodloader.com/cb/PolyTrackMods/PolyModLoader/0.6.0/PolyTypes.js";
-
+  
 class PolyProxy extends PolyMod {
   preInit = (pml) => {
     pml.registerSettingCategory("PolyProxy");
     pml.registerSetting(
-      "Proxy Url",
-      "ppurl",
-      SettingType.CUSTOM,
-      "https://polyproxy.polymodloader.com/",
-      [
-        { title: "Official", value: "https://polyproxy.polymodloader.com/" },
-        { title: "Rxrk", value: "https://proxy.rxk.workers.dev/" },
-        { title: "3", value: "https://value3test.com/" },
-      ],
-    );
-    this.url = () => {
-      return pml.getSetting("ppurl");
-    };
+     "Proxy Url",
+     "ppurl",
+    SettingType.CUSTOM,
+     "https://polyproxy.polymodloader.com/",
+  [
+    { title: "Official",  value: "https://polyproxy.polymodloader.com/" },
+    { title: "Rxrk",     value: "https://proxy.rxk.workers.dev/" },
+    { title: "3",       value: "https://value3test.com/" },
+  ]
+);
+    this.url = () => {return pml.getSetting("ppurl")}
 
     pml.registerGlobalMixin({
       type: MixinType.REPLACEBETWEEN,
@@ -32,3 +30,4 @@ class PolyProxy extends PolyMod {
 }
 
 export let polyMod = new PolyProxy();
+
