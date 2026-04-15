9b5a6a90e798bffe20d252f9cdd70d8823ece6a1
NotRx <Anonymous@proton.me>
2026-04-15 13:27:17 +0100

    grotgo

--- 3decspeed/1.0.0/main.mod.js
+++ 3decspeed/1.0.0/main.mod.js
@@ -25,7 +25,7 @@ class ThreeDecimalSpeedometer extends PolyMod {
     //   tokenStart: `const i = Math.trunc(n).toString();`,
     //   tokenEnd: `const i = Math.trunc(n).toString();`,
     //   func: `const i = n.toFixed(Number.parseInt(ActivePolyModLoader.getSetting("speedometer")) ?? 0);`,
-    // });
+    // });w
   };
 }
 export let polyMod = new ThreeDecimalSpeedometer();
